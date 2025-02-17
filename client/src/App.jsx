32bc63254605cfac0786c8e4fcd1898cd5af9404
VeenusLynn <l.noura@esi-sba.dz>
2025-02-17 18:10:06 +0800

    added login and register skeleton pages (needs restyling later)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
-import { Toaster } from "sonner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -11,7 +13,10 @@ function App() {
         <Routes>
           <Route path="/" element={<UserLayout />}>
             <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
           </Route>
+          <Route path="" element={<Login />}></Route>
           <Route>{/* Admin Layout */}</Route>
         </Routes>
       </BrowserRouter>
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import registerImg from "../assets/collection2.jpg";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("User Registered : ", { name, email, password });
+  };
+
+  return (
+    <div className="flex">
+      <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md bg-white p-8 rounded-lg border shadow-sm"
+        >
+          <div className="flex justify-center mb-6">
+            <h2 className="text-xl font-medium">SwiftCart</h2>
+          </div>
+          <h2 className="text-2xl font-bold text-center mb-6">Hey there!</h2>
+          <p className="text-center mb-6">Create a new account</p>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Name</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full p-2 border rounded"
+              placeholder="Enter you name"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-2 border rounded"
+              placeholder="Enter you email address"
+            />
+          </div>
+          <div className="mb-4 ">
+            <label className="block text-sm font-semibold mb-2">Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 border rounded"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transiiton-all duration-400"
+          >
+            Register
+          </button>
+          <p className="mt-6 text-center text-sm">
+            Already have an account?{" "}
+            <Link to="/Login" className="text-blue-500">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+      <div className="hidden md:block w-1/2 bg-gray-800">
+        <div className="h-full flex flex-col justify-center items-center">
+          <img
+            src={registerImg}
+            alt="SwiftCart"
+            className="h-[660px] w-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
